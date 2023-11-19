05a5bb651445f04454f78d7da95f7b9bea4cdf9b
wishalfe <144664605+wishalfe@users.noreply.github.com>
2023-11-19 09:46:11 +0700

    Add files via upload

---
+++ web_statis/uts.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+alert("Selamat Datang");
+
+const menu = document.querySelector(".navbar #menu");
+const nav = document.querySelector(".navbar .navbar-nav");
+
+menu.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
